a3e7d1f12c20fb7a051129ba65df990957b5c508
huanyueyaoqin <huanyueyaoqin@qq.com>
2020-06-01 23:25:06 +0800

    feat:promiseError监控

--- src/monitor/lib/jsError.js
+++ src/monitor/lib/jsError.js
@@ -8,19 +8,51 @@ export function injectJsError() {
     console.log(event);
     let lastEvent = getLastEvent(); //获得最后一个交互事件
     console.log(lastEvent);
-    let log = {
+    tracker.send({
       kind: "stability", //监控指标的大类
       type: "error", //小类型 这是一个错误
       errorType: "jsError", //JS执行出错
-      url: "", //访问哪个路径报错了
       message: event.message, //报错信息
       filename: event.filename, //哪个文件报错了
       position: `${event.lineno}:${event.colno}`, //哪一行列报错
       stack: getLines(event.error.stack),
       selector: lastEvent ? getSelector(lastEvent.path) : "", //代表最后一个操作的元素
-    };
-    // console.log(log);
-    tracker.send(log);
+    });
+  });
+
+  window.addEventListener("unhandledrejection", (event) => {
+    console.log(event);
+    let lastEvent = getLastEvent(); //最后一个交互事件
+    let message;
+    let filename;
+    let line = 0;
+    let column = 0;
+    let stack = "";
+    let reason = event.reason;
+    if (typeof reason === "string") {
+      message = reason;
+    } else if (typeof reason === "object") {
+      message = reason.message;
+      //错误对象
+      if (reason.stack) {
+        let matchResult = reason.stack.match(/at\s+(.+):(\d+):(\d+)/);
+        filename = matchResult[1];
+        line = matchResult[2];
+        column = matchResult[3];
+      }
+
+      stack = getLines(reason.stack);
+    }
+    tracker.send({
+      kind: "stability", //监控指标的大类
+      type: "error", //小类型 这是一个错误
+      errorType: "promiseError", //JS执行出错
+      message, //报错信息
+      filename, //哪个文件报错了
+      position: `${line}:${column}`, //哪一行列报错
+      stack,
+      selector: lastEvent ? getSelector(lastEvent.path) : "", //代表最后一个操作的元素
+    });
   });
 }
 
